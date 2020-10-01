db1024f93ac60d66752891aa2e162e15411917ad
trstcod <saurabh.sheshank@gmail.com>
2020-10-01 19:16:02 +0530

    random

---
+++ copy/falsy.js
@@ -0,0 +1,12 @@
+var user=undefined;
+if(user)
+console.log("TRUE");
+else
+
+console.log("FFFalse");
+
+var us="2";
+if(2==us)
+console.log("Coercion");
+
+//=== so that JS dont assume on its own
---
+++ copy/script.js
@@ -0,0 +1,21 @@
+var fullName="Saurabh Gupta";
+const uid=123;
+var email="saurabh.sheshank@gmail.com";
+var pass="Saurabh@123";
+
+//console.log("FUll name is :"+ fullName);
+/*
+console.log(`
+With user id : ${uid}
+!Name is : ${fullName}
+?email : ${email}
+and uses password :${pass}
+`);
+
+todo i have to complete this shit 
+*/
+
+var list=799;
+var sell=199;
+var dis=Math.round((list-sell)/list * 100); //!may be an alert
+console.log(dis+ "% off");
