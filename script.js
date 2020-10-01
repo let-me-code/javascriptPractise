c14969ea0cb67e553f4d7dd3fba9c90e0950b148
trstcod <saurabh.sheshank@gmail.com>
2020-10-01 17:26:26 +0530

    mmm

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ var email="saurabh.sheshank@gmail.com";
 var pass="Saurabh@123";
 
 //console.log("FUll name is :"+ fullName);
+/*
 console.log(`
 With user id : ${uid}
-Name is : ${fullName}
-email : ${email}
+!Name is : ${fullName}
+?email : ${email}
 and uses password :${pass}
 `);
 
+todo i have to complete this shit 
+*/
+
 var list=799;
 var sell=199;
-var dis=Math.round((list-sell)/list * 100);
+var dis=Math.round((list-sell)/list * 100); //!may be an alert
 console.log(dis+ "% off");
